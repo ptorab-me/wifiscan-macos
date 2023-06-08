e601a9d8a185514e5b41b1560fd0c41974b2ce13
Payam Torab <ptorab@parosatech.com>
2023-06-08 11:50:57 -0700

    Initial Commit

---
+++ WiFiScan/WiFiScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiFiScanApp.swift
+//  WiFiScan
+//
+//  Created by Payam Torab on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct WiFiScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
